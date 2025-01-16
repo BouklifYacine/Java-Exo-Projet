3e2f444137f3df206d571d485ea8aed3e12839ce
YacineBoukliff <yacine69800@live.fr>
2025-01-16 12:16:45 +0100

    Projet fini

--- app/Bibliothèque.java
+++ app/Bibliothèque.java
@@ -10,41 +10,42 @@ public class Bibliothèque {
     }
 
     public void AjouterLivre(Livre livre) {
-        TotalLivres.add(livre);
-        System.out.println(" Voici le nouveau livre : " + livre.getTitre());
-
+        if (TotalLivres.size() < 3) { 
+            TotalLivres.add(livre);
+            System.out.println("Voici le nouveau livre : " + livre.getTitre());
+        } else {
+            System.out.println("Vous ne pouvez pas ajouter plus de 3 livres !");
+        }
     }
 
- public void RetirerLivre(Livre livre){
-    TotalLivres.remove(livre)
-    System.out.println("Le livre retiré est : " + livre.getTitre());
- }
+    public void RetirerLivre(Livre livre) {
+        if (TotalLivres.remove(livre)) { 
+            System.out.println("Le livre retiré est : " + livre.getTitre());
+        } else {
+            System.out.println("Le livre n'a pas été trouvé dans la bibliothèque.");
+        }
+    }
 
     public void AfficherLivres() {
         if (TotalLivres.isEmpty()) {
-            System.out.println(" Votre bibliothèque est VIDE !!! ");
+            System.out.println("Votre bibliothèque est VIDE !!!");
         } else {
-            System.out.println("Le livre ajouté est : ");
+            System.out.println("Livres dans la bibliothèque : ");
             for (Livre nouveaulivre : TotalLivres) {
                 nouveaulivre.AfficherInfos();
             }
         }
     }
 
-   
-
- public void NombreLivre(){
-    
-    int nombre = TotalLivres.size();
-    if (TotalLivres.isEmpty()){
-        System.out.println("Je vois que vous avez " + TotalLivres.size() +  " Livres ");
-        System.out.println("Rajouter un livre !!! ");
+    public void NombreLivre() {
+        int nombre = TotalLivres.size();
+        if (nombre == 0) {
+            System.out.println("Je vois que vous avez " + nombre + " livre(s).");
+            System.out.println("Rajoutez un livre !!!");
+        } else if (nombre <= 3) {
+            System.out.println("Vous avez " + nombre + " livre(s).");
+        } else {
+            System.out.println("Vous ne pouvez pas stocker plus de 3 livres !");
+        }
     }
-    else if(!TotalLivres.isEmpty()){
-        System.out.println("Vous avez " + TotalLivres.size() + " livres");
-    }
-   else if (nombre > 3){
-System.out.println(" Vous pouvez stockez au maximum 33 livres");
-   }
- }
 }
--- app/Main.java
+++ app/Main.java
@@ -5,15 +5,16 @@ public class Main {
         Bibliothèque bibliothèque = new Bibliothèque();
 
         Livre livre1 = new Livre("Javascript", "Javascript guy", 2099);
-        Livre livre2 = new Livre("Csgo", "Gaben", 1990);
-        Livre livre3 = new Livre("Csgo", "Gaben", 1990);
-        Livre livre4 = new Livre("Csgo", "Gaben", 1990);
+        Livre livre2 = new Livre("Messi", "Gaben", 1990);
+        Livre livre3 = new Livre("Neymar", "Gaben", 1990);
+        Livre livre4 = new Livre("Ronaldo", "Gaben", 1990);
 
         livre1.AfficherInfos();
 
         bibliothèque.AjouterLivre(livre1);
         bibliothèque.AjouterLivre(livre2);
-        bibliothèque.AjouterLivre(livre3);
+        bibliothèque.AjouterLivre(livre3);;
+        bibliothèque.RetirerLivre(livre1);
         bibliothèque.AjouterLivre(livre4);
         bibliothèque.AfficherLivres();
         bibliothèque.NombreLivre();
