7f41340cb1f2a00d903acd2c075aad87954d10e0
YacineBoukliff <yacine69800@live.fr>
2025-01-16 10:42:08 +0100

    Premier exo constructeur classe méthode basique

---
+++ app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        Personne yacine = new Personne("Yacine", 25, "Saint-Priest");
+        Personne Norhane = new Personne("Norhane", 22, "Vaulx-en-Velin");
+        yacine.SePresenter();
+        Norhane.SePresenter();
+        
+    }
+}
---
+++ app/Personne.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Personne {
+    String nom; 
+    int age; 
+    String ville; 
+
+     Personne(String nom , int age , String ville){
+        this.nom = nom; 
+        this.age = age; 
+        this.ville = ville;
+    }
+
+    void SePresenter(){
+        System.out.println("Je m'appelle " + this.nom  + " j'ai " + this.age + " ans" + " et j'habite a " + this.ville);
+    }
+}
