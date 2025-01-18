dfcb4d614e6dad20fcb15dd70f246a0bdb0772d2
YacineBoukliff <yacine69800@live.fr>
2025-01-18 12:15:09 +0100

    Fichier parent Personne faite

--- app/Projet4/Main.java
+++ app/Projet4/Main.java
@@ -3,17 +3,17 @@ package app.Projet4;
 public class Main {
     public static void main(String[] args) {
 
-        Etudiant etudiant1 = new Etudiant("Yacine", 25, 1, "Licence");
+        // Etudiant etudiant1 = new Etudiant("Yacine", 25, 1, "Licence");
 
-        Professeur professeur1 = new Professeur("Gérard", 50, 2, "Maths");
+        // Professeur professeur1 = new Professeur("Gérard", 50, 2, "Maths");
 
-        Cours cours1 = new Cours("Geo", professeur1);
-        Cours cours2 = new Cours("Algèbre", professeur1);
+        // Cours cours1 = new Cours("Geo", professeur1);
+        // Cours cours2 = new Cours("Algèbre", professeur1);
 
-        cours1.inscrireEtudiant(etudiant1);
+        // cours1.inscrireEtudiant(etudiant1);
 
-        cours1.AfficherEtudiantInscrit();
+        // cours1.AfficherEtudiantInscrit();
 
-        professeur1.AfficherCoursEnseignés();
+        // professeur1.AfficherCoursEnseignés();
     }
 }
--- app/Projet4/Personne.java
+++ app/Projet4/Personne.java
@@ -1,24 +1,29 @@
 package app.Projet4;
 
 public class Personne {
-    private String nom;
-    private int age;
-    private int id;
 
-    public Personne(String nom, int age, int id ){
-        this.nom = nom; 
-        this.age = age; 
-        this.id = id;
-    }
+   private String nom; 
+   private int age ;
+   private int id; 
 
-    public String getNom(){
-        return nom;
-    }
-    public int getAge(){
-        return age;
-    }
-    public int getId(){
-        return id;
-    }
+   public Personne(String nom, int age, int id){
+    this.nom = nom;
+    this.age = age; 
+    this.id = id; 
+   }
+
+   public String getNom(){
+    return nom;
+   }
+
+   public int getAge(){
+    return age;
+   }
+
+   public int getId(){
+    return id; 
+   }
+
+
     
 }
